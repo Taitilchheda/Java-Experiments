7f58f5fb35c6ab7fddc4c2022f8f13be9271db01
Taitil Chheda <taitilchheda@gmail.com>
2024-03-18 22:20:29 +0530

    java exp 1

---
+++ Numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+	System.out.println("Enter a number");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        if (n % 2 != 0) {
+            System.out.println("Weird");
+        } else if (n >= 2 && n <= 5) {
+            System.out.println("Not Weird");
+        } else if (n >= 6 && n <= 20) {
+            System.out.println("Weird");
+        } else if (n > 20) {
+
+            System.out.println("Not Weird");
+        }
+    }
+}
